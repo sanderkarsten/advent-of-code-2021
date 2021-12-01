b57f08a5c6dce1f0bdf0134aaf085948a7cec4a8
sanderkarsten <sander_karsten@hotmail.com>
2021-12-01 09:29:07 +0100

    added second assignment

--- day1/day1_2.hs
+++ day1/day1_2.hs
@@ -1,13 +1,13 @@
-module Day1_1 where
+module Day1_2 where
 import Data.List
 
 count [] = 0
-count(x:y:xs) | x < y = 1 + count (y:xs)
+count(a:b:c:d:xs) | a+b+c<b+c+d = 1 + count (b:c:d:xs)
 count (x:xs) = count xs
 
 strToIntList x = map (\x -> read x::Int) (lines x)
 
 main :: IO()
 main = do 
-    input <- readFile "input.txt"
+    input <- readFile "input_2.txt"
     putStr . show $ count (strToIntList input)
