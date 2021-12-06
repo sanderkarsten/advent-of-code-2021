7e8b229cf12d5ba1fbf3a1641357edc124bbb1d2
sanderkarsten <sander_karsten@hotmail.com>
2021-12-06 11:27:41 +0100

    added day6

---
+++ day6/day6.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+module Day06 where
+
+import Data.List (group, sort)
+import qualified Data.Text as T
+import Data.Map (fromListWith, toList)
+
+strToInt x = read x::Int
+
+splitStringOn x xs = map T.unpack $ T.splitOn (T.pack x) (T.pack xs)
+
+parseInput = map strToInt . splitStringOn ","
+
+shift (x : xs) = let (y, [a, b]) = splitAt 6 xs in y ++ [a + x] ++ [b, x]
+
+initState = map (pred . length) . group . sort . (++ [0 .. 8])
+
+main = do 
+    input <- readFile "input.txt"
+    print $ sum ((!! 80) (iterate shift (initState (parseInput input))))
+    print $ sum ((!! 256) (iterate shift (initState (parseInput input))))
