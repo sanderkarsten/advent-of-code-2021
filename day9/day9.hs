7c762f2457d630b7d97714366f4c54bd2601be76
sanderkarsten <sander_karsten@hotmail.com>
2021-12-09 21:27:59 +0100

    made more readable

--- day9/day9.hs
+++ day9/day9.hs
@@ -1,8 +1,4 @@
--- {-# LANGUAGE OverloadedStrings #-}
-import Data.List (group, sort)
-import qualified Data.Text as T
-import Data.Map (fromListWith, toList)
-import Data.Maybe
+import Data.List (sort)
 
 strToInt :: String->Int
 strToInt x = read x::Int
@@ -11,37 +7,32 @@ charToInt = strToInt . (:"")
 
 parseInput xs = map (map charToInt) (lines xs)
 
-dataAt _ [] = Nothing
-dataAt y x | y < 0 = Nothing
-dataAt y (x:xs)  | y <= 0 = Just x
-                 | otherwise = dataAt (y-1) xs
-
-iterateOverMat row column mat | not (null mat) && length (head mat) == column = iterateOverMat (row+1) 0 mat
-iterateOverMat row column mat | length mat == row = 0
-iterateOverMat row column mat = let currentRow = mat !! row in let currentVal = currentRow !! column in 
-    if  maybe True (\ nextRow -> currentVal < (nextRow !! column)) (dataAt (row + 1) mat) &&
-        maybe True (\prevRow -> currentVal < (prevRow !! column)) (dataAt (row - 1) mat) &&
-        maybe True (currentVal <) (dataAt (column+1) currentRow) &&
-        maybe True (currentVal <) (dataAt (column-1) currentRow)
-      then 1+currentVal + iterateOverMat row (column+1) mat else iterateOverMat row (column+1) mat
-
-getElem m (x, y) | 0 <= y && y < length m && 0 <= x && x < (length. head) m = m !! y !! x
+getElem :: [[Int]] -> (Int,Int)-> Int
+getElem mat (row,col) | 0 <= col && col < length mat && 0 <= row && row < (length. head) mat = mat !! col !! row
 getElem _ _ = 9
 
-neighbours (x, y) = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+neighbours :: (Int, Int) -> [(Int, Int)]
+neighbours (row, col) = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
 
-isPointMin (x, y) m = all ((> getElem m (x,y)) . getElem m) (neighbours (x,y))
+findMinPoints :: Int->Int -> [[Int]] -> [(Int, Int)]
+findMinPoints row col mat | row < (length . head) mat && all ((> getElem mat (row, col)) . getElem mat) (neighbours (row, col)) = (row, col) : findMinPoints (row + 1) col mat
+findMinPoints row col mat | row < (length . head) mat = findMinPoints (row + 1) col mat
+findMinPoints row col mat | col < length mat = findMinPoints 0 (col + 1) mat
+findMinPoints _ _ _  = []
 
-findMinPoints (x, y) m | x < (length . head) m && isPointMin (x,y) m = (x,y) : findMinPoints (x + 1, y) m
-findMinPoints (x, y) m | x < (length . head) m = findMinPoints (x + 1, y) m
-findMinPoints (x, y) m | y < length m = findMinPoints (0, y + 1) m
-findMinPoints _ _  = []
+-- Part 1:
+getRiskLevel :: [[Int]] -> Int
+getRiskLevel mat = sum (map ((+1) . getElem mat) (findMinPoints 0 0 mat))
 
-findBasin m s p | getElem m p < 9 && notElem p s = foldl (findBasin m) (p:s) (neighbours p)
-findBasin _ s _ = s
+-- Part 2:
+findBasin :: [[Int]] -> [(Int, Int)] -> (Int,Int) -> [(Int, Int)]
+findBasin mat seen point | getElem mat point < 9 && notElem point seen = foldl (findBasin mat) (point:seen) (neighbours point)
+findBasin _ seen _ = seen
 
-findBasins m x =  product $ drop (length x - 3) (sort (map ((sum . map length) . findBasin m []) x))
+findBasins :: [[Int]] -> [(Int, Int)] -> Int
+findBasins mat min =  product $ drop (length min - 3) (sort (map ((sum . map length) . findBasin mat []) min))
 
 main = do 
     input <- readFile "input.txt"
-    print $ findBasins (parseInput input) (findMinPoints (0,0) (parseInput input))
+    print $ getRiskLevel (parseInput input)
+    print $ findBasins (parseInput input) (findMinPoints 0 0 (parseInput input))
